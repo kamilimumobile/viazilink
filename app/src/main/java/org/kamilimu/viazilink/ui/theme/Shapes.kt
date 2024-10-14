34bde7ed34d5bc6f1fba398ae2467a733beb8282
Hansel Omondi <ogopabhs@gmail.com>
2024-10-14 20:41:43 +0300

    Add: material theme for the app

---
+++ app/src/main/java/org/kamilimu/viazilink/ui/theme/Shapes.kt
@@ -0,0 +1,11 @@
+package org.kamilimu.viazilink.ui.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Shapes
+import androidx.compose.ui.unit.dp
+
+val Shapes = Shapes(
+    large = RoundedCornerShape(20.dp),
+    medium = RoundedCornerShape(16.dp),
+    small = RoundedCornerShape(12.dp)
+)
