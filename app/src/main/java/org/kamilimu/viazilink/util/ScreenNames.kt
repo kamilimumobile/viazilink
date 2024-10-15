221546ef54cc712321331eea8803e685a3d0d5b3
Hansel Omondi <ogopabhs@gmail.com>
2024-10-16 02:09:07 +0300

    Add: AuthStatus data class and screen names

---
+++ app/src/main/java/org/kamilimu/viazilink/util/ScreenNames.kt
@@ -0,0 +1,7 @@
+package org.kamilimu.viazilink.util
+
+enum class ScreenNames(val route: String) {
+    LoginScreen(route = "Login"),
+    SignUpScreen(route = "Sign Up"),
+    HomeScreen(route = "ViaziLink")
+}
