53ce13a84ef33b4127e34d63c382e3b374c9d673
Hansel Omondi <ogopabhs@gmail.com>
2024-10-21 16:08:57 +0300

    Update: ProduceListingsPage to display listings
    This page displays a farmer's listings. The ListingCard component that
    will contain the image of the produce in the listing as well as its
    details has also been implemented

--- app/src/main/java/org/kamilimu/viazilink/farmer/presentation/ProduceListingsPage.kt
+++ app/src/main/java/org/kamilimu/viazilink/farmer/presentation/ProduceListingsPage.kt
@@ -3,25 +3,44 @@
 package org.kamilimu.viazilink.farmer.presentation
 
 import android.content.res.Configuration
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import org.kamilimu.viazilink.R
+import org.kamilimu.viazilink.farmer.domain.model.Listing
+import org.kamilimu.viazilink.farmer.presentation.components.ListingCard
 import org.kamilimu.viazilink.ui.theme.AppTheme
+import org.kamilimu.viazilink.util.ScreenViewState
+import org.kamilimu.viazilink.util.components.LoadingIndicator
 
 @Composable
-fun ProduceListingsPage(scrollBehavior: TopAppBarScrollBehavior) {
+fun ProduceListingsPage(
+    scrollBehavior: TopAppBarScrollBehavior,
+    listingsPageViewModel: ProduceListingsViewModel = hiltViewModel()
+) {
+    val screenState by listingsPageViewModel.listingsUiState.collectAsStateWithLifecycle()
+
     ProduceListingsContent(
         scrollBehavior = scrollBehavior,
+        screenState = screenState,
         modifier = Modifier
             .fillMaxSize()
             .padding(dimensionResource(R.dimen.padding_small))
@@ -31,15 +50,48 @@ fun ProduceListingsPage(scrollBehavior: TopAppBarScrollBehavior) {
 @Composable
 private fun ProduceListingsContent(
     modifier: Modifier = Modifier,
-    scrollBehavior: TopAppBarScrollBehavior
+    scrollBehavior: TopAppBarScrollBehavior,
+    screenState: ScreenViewState
 ) {
     val scrollState = rememberLazyListState()
 
-    LazyColumn(
-        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
-        state = scrollState
-    ) {
-        /*TODO: Fetch all listings posted by a farmer*/
+    when (screenState) {
+        is ScreenViewState.Loading -> {
+            Column(
+                modifier = modifier,
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                LoadingIndicator()
+            }
+        }
+
+        is ScreenViewState.Failure -> {
+            Column(
+                modifier = modifier,
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text(
+                    text = screenState.message,
+                    style = MaterialTheme.typography.bodyLarge
+                )
+            }
+        }
+
+        is ScreenViewState.Success<*> -> {
+            LazyColumn(
+                modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+                state = scrollState
+            ) {
+                val listings = screenState.data as List<Listing>
+                items(listings) { listing ->
+                    ListingCard(listing = listing)
+                }
+            }
+        }
+
+        else -> Unit
     }
 }
 
@@ -58,6 +110,7 @@ private fun ProduceListingsPreview() {
     AppTheme {
         ProduceListingsContent(
             scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior(),
+            screenState = ScreenViewState.Failure("No Records Found"),
             modifier = Modifier
                 .fillMaxSize()
                 .padding(dimensionResource(R.dimen.padding_small))
